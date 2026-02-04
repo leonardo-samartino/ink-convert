6c903dec35b19181cd619e466f856c73f05d91c1
Leonardo G. Samartino <112051738+leonardo-samartino@users.noreply.github.com>
2026-02-04 15:55:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
 export default defineConfig(({ mode }) => ({
   base: "/ink-convert/",
   server: {
